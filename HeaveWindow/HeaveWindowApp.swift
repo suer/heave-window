70b1c8cce6a44c6f9280ecd92d8e481618cddc95
Ryo SUETSUGU <suetsugu.r@gmail.com>
2026-01-16 22:31:25 +0900

    Initial Commit

---
+++ HeaveWindow/HeaveWindowApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeaveWindowApp.swift
+//  HeaveWindow
+//
+//  Created by Ryo Suetsugu on 2026/01/16.
+//
+
+import SwiftUI
+
+@main
+struct HeaveWindowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
